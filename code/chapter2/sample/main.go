f30e67854de17e06f2610a5d8585583546f19a8a
盛佳 <1368329355@qq.com>
2019-06-05 11:52:39 +0800

    update the url

--- code/chapter2/sample/main.go
+++ code/chapter2/sample/main.go
@@ -4,8 +4,8 @@ import (
 	"log"
 	"os"
 	//这里的下划线是为了让GO语言对包做初始化操作，但是并不使用包里的标识符。这样做的目的是调用matchers包中的rss.go中init函数。
-	_ "github.com/goinaction/code/chapter2/sample/matchers"
-	"github.com/goinaction/code/chapter2/sample/search"
+	_ "github.com/GO_In_Action/code/chapter2/sample/matchers"
+	"github.com/GO_In_Action/code/chapter2/sample/search"
 )
 
 // init is called prior to main.
