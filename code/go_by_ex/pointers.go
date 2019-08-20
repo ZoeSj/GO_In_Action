98fc210132c55786bcfb8146509d7f8707eb6056
盛佳 <1368329355@qq.com>
2019-08-20 14:59:02 +0800

    [example] some example about go grammar

--- code/go_by_ex/pointers.go
+++ code/go_by_ex/pointers.go
@@ -1,5 +1,23 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	i := 1
+	fmt.Println("initial:", i)
+
+	zeroval(i)
+	fmt.Println("zeroval:", i)
+
+	zeroptr(&i)
+	fmt.Println("zeroptr:", i)
+
+	fmt.Println("pointer:", &i)
+
+}
+func zeroptr(i *int) {
+	*i = 0
+}
+func zeroval(i int) {
+	i = 0
 }
